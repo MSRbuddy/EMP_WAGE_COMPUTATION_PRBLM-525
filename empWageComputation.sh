19942bbd75e7f22d2195b4cc8e9011594500d7ad
MSRbuddy <meghanadasari222@gmail.com>
2023-01-13 15:57:34 +0530

    Added: Caliculation of Emp Daily Wage using Switch-Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -46,3 +46,28 @@ else
 fi
 salary=$(($perHourSalary * $workingHour));
 echo "Employee has earned $salary $ today";
+
+#UC-4 -> Calculate Emp Daily Wage using Switch-Case
+
+isPresent=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+case $isPresent in
+	0)
+	echo "Employee is absent";
+	workingHour=0;
+	;;
+
+	1)
+	echo "Employee is present";
+	workingHour=8;
+	;;
+
+	2)
+	echo "Employee is working as part time";
+	workingHour=4;
+	;;
+esac
+salary=$(($perHourSalary * $workingHour));
+echo "Employee has earned $salary $ today";
+
