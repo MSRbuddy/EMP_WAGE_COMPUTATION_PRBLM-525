78eff3a0bbf5eed72559bfb8c08756414a57d123
MSRbuddy <meghanadasari222@gmail.com>
2023-01-13 16:08:46 +0530

    Added: Caliculation of Emp Wage for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -71,3 +71,34 @@ esac
 salary=$(($perHourSalary * $workingHour));
 echo "Employee has earned $salary $ today";
 
+#UC-5 -> Calculate Emp Wage for a Month
+
+perHourSalary=20;
+workingHour=0;
+totalSalary=0;
+
+for ((day=1; day<=20 ;day++))
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
+
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+done
+
+echo "Employee has earned $totalSalary $ in a month";
+
